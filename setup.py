1718f018723065af9bef5c5a1dc26bfa6ebe5535
filwie <filip.wiechec@gmail.com>
2019-09-22 16:15:10 +0200

    Remove unused function

--- setup.py
+++ setup.py
@@ -5,35 +5,26 @@ from typing import List
 REPO_ROOT = Path(__file__).parent
 
 
-def readme() -> str:
+def readme():
     readme = REPO_ROOT / 'README.md'
     if readme.exists():
         return readme.read_text()
     return ''
 
-
-def requirements() -> List[str]:
-    requirements = REPO_ROOT / 'requirements.txt'
-    if requirements.exists():
-        r = requirements.read_text()
-        return r.split()
-    return []
-
-
 setup(name='colorlogging',
       version='0.0.1',
-      description='Logging formatter ',
+      description='ColorFormatter class for logging module.',
       long_description=readme(),
       classifiers=[
           'Programming Language :: Python :: 3',
           'License :: OSI Approved :: MIT License',
       ],
-      keywords='',
-      url='',
-      author='',
-      author_email='',
+      keywords='logging color',
+      url='https://github.com/filwie/cologging',
+      author='Filip Wiechec',
+      author_email='filip.wiechec@gmail.com',
       license='MIT',
       packages=find_packages(),
-      install_requires=requirements(),
+      install_requires=['incolor'],
       include_package_data=True,
       zip_safe=False)
